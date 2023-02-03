9c3077ee73b59ce7e01256fec74eec43e07f60d4
Tong Zhou <tz@gatech.edu>
2023-02-03 20:00:52 +0000

    [Day2] Naive implementation of exp in cuda

--- v1.py
+++ v1.py
@@ -1,43 +1,29 @@
 import torch
 import cupy as cp
-from cupyx.profiler import benchmark
-import nanopyc_day1 as nanopyc
-
+from cupyx.profiler import benchmark as cupybench
+import nanopyc_day2 as nanopyc
+import torch.utils.benchmark as torchbench
 
 @nanopyc.jit
-def softmax(a):
+def my_exp(a):
     b = torch.exp(a)
     return b
 
-# def softmax(a):
-#     M, N = a.shape
-#     b = torch.empty_like(a)
-
-    # _a = cp.asarray(a)
-    # _b = cp.asarray(b)
-
-    # src = open('v1_kernel.cu').read()
-    # kernel = cp.RawKernel(src, 'kernel', backend='nvcc', options=('-O3',))
-    # nthreads = 128
-    # nblocks = ceil_div(M, nthreads)
-    # kernel(
-    #     (nblocks,), 
-    #     (nthreads,), 
-    #     (M, N, _a, _b)
-    # )
-
-    # print(benchmark(lambda: kernel(
-    #     (nblocks,), 
-    #     (nthreads,), 
-    #     (M, N, _a, _b)
-    # )))
+def torch_exp(a):
+    b = torch.exp(a)
     return b
 
+def bench(fn):
+    t0 = torchbench.Timer(
+    stmt='fn()',
+    globals={'fn': fn})
+    return t0.timeit(20).mean * 1000
 
-for shape in [(1024, 1024)]:
+for shape in [(10240, 1024)]:
     M, N = shape
     a = torch.randn(M, N, device='cuda', dtype=torch.float32)
-    b = softmax(a)
-    print(torch.allclose(a+1, b))
-
-    print(benchmark(lambda: softmax(a)))
+    b_torch = torch_exp(a)
+    b_my = my_exp(a)
+    print(torch.allclose(b_my, b_torch))
+    print(bench(lambda: my_exp(a)))
+    print(bench(lambda: torch_exp(a)))
--- v1_kernel.cu
+++
@@ -1,7 +0,0 @@
-extern "C" __global__
-void kernel(int M, int N, float* a, float* b) {
-    int m = blockDim.x * blockIdx.x + threadIdx.x;
-    for (int i = 0; i < N; i++) {
-        b[m*N + i] = a[m*N + i] + 1;
-    }
-}
