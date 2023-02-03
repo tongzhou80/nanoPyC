be1f9b6ced236ad97a779a4675d9921d0fc97ae9
Tong Zhou <tz@gatech.edu>
2023-02-03 19:59:31 +0000

    [Day2] Naive implementation of exp in cuda

---
+++ kernels/exp_v1.cu
@@ -0,0 +1,7 @@
+extern "C" __global__
+void kernel(int M, int N, float* a, float* b) {
+    int m = blockDim.x * blockIdx.x + threadIdx.x;
+    for (int i = 0; i < N; i++) {
+        b[m*N + i] = exp(a[m*N + i]);
+    }
+}
