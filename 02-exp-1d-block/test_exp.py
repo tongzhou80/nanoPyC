c4de9c976a393ebb96959e7aacadce35e8220409
Tong Zhou <tz@gatech.edu>
2023-03-19 21:51:33 +0000

    added cpu timing

--- 02-exp-1d-block/test_exp.py
+++ 02-exp-1d-block/test_exp.py
@@ -13,15 +13,19 @@ def torch_exp(a):
 
 def bench(fn):
     t0 = torchbench.Timer(
-    stmt='fn()',
-    globals={'fn': fn})
+        stmt='fn()',
+        globals={'fn': fn},
+        num_threads=torch.get_num_threads()
+    )
     return t0.timeit(20).mean * 1000
 
-for shape in [(10240, 2048)]:
+for shape in [(10240, 2048), (102400, 2048)]:
     M, N = shape
     a = torch.randn(M, N, device='cuda', dtype=torch.float32)
+    a_cpu = a.cpu()
     b_torch = torch_exp(a)
     b_my = my_exp(a)
     print('allclose:', torch.allclose(b_my, b_torch))
     print('torch runtime:', bench(lambda: torch_exp(a)), 'ms')
+    print('torch (cpu) runtime:', bench(lambda: torch_exp(a_cpu)), 'ms')
     print('  our runtime:', bench(lambda: my_exp(a)), 'ms')
--- 08-reduce-warp/test_reduce.py
+++ 08-reduce-warp/test_reduce.py
@@ -15,16 +15,20 @@ def torch_kernel(a):
 
 def bench(fn):
     t0 = torchbench.Timer(
-    stmt='fn()',
-    globals={'fn': fn})
+        stmt='fn()',
+        globals={'fn': fn},
+        num_threads=torch.get_num_threads()
+    )
     return t0.timeit(20).mean * 1000
 
 for shape in [(10240, 2048), (10240, 2048*2)]:
     M, N = shape
     a = torch.ones(M, N, device='cuda', dtype=torch.float32)
+    a_cpu = a.cpu()
     out_torch = torch_kernel(a)
     out_my = my_kernel(a)
     print('allclose:', torch.allclose(out_my, out_torch))
     print('torch runtime:', bench(lambda: torch_kernel(a)), 'ms')
+    print('torch (cpu) runtime:', bench(lambda: torch_kernel(a_cpu)), 'ms')
     print('  our runtime:', bench(lambda: my_kernel(a)), 'ms')
 
